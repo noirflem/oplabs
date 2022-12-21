74f988f1193ba6535002a7390bd78f16ab7c0afb
noirflem <112944219+noirflem@users.noreply.github.com>
2022-12-21 22:39:29 +0300

    Update functionsLab11.c

--- lab11/functionsLab11.c
+++ lab11/functionsLab11.c
@@ -1,17 +1,17 @@
 #include "math.h"
 #include "lab11header.h"
 
-float P(struct Parallelogram ParCoords){
+float P(struct Parallelogram *ParCoords){
 
     float a, b;
-    a = abs(ParCoords.x1 - ParCoords.x2);
-    b = sqrt(abs(ParCoords.y2 - ParCoords.y3) * abs(ParCoords.y2 - ParCoords.y3) + (abs(ParCoords.x2 - ParCoords.x3) * abs(ParCoords.x2 - ParCoords.x3)));
+    a = abs(ParCoords->x1 - ParCoords->x2);
+    b = sqrt(abs(ParCoords->y2 - ParCoords->y3) * abs(ParCoords->y2 - ParCoords->y3) + (abs(ParCoords->x2 - ParCoords->x3) * abs(ParCoords->x2 - ParCoords->x3)));
     return (a + b) * 2;
 
 }
 
-int S(struct Parallelogram ParCoords){
+int S(struct Parallelogram *ParCoords){
 
-    return (ParCoords.y1 - ParCoords.y4) * (ParCoords.x2 - ParCoords.x1);
+    return (ParCoords->y1 - ParCoords->y4) * (ParCoords->x2 - ParCoords->x1);
 
 }
